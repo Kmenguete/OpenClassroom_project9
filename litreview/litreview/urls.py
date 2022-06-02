d13f2e24256a10143169e23f9f2da609df26786f
Kmenguete <menguetekevin@gmail.com>
2022-06-02 17:04:14 +0200

    creating the home page for connected users

--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 import authentication.views
+import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', authentication.views.login_page, name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', reviews.views.home, name='home'),
 ]

a7663ac45d47ff4cdd0ce996f2e7841d81f894bf
Kmenguete <menguetekevin@gmail.com>
2022-06-03 10:18:28 +0200

    adding the signup feature

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
 
 from . import forms
 
@@ -26,3 +27,14 @@ def logout_user(request):
 
     logout(request)
     return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})

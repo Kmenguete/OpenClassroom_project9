c4e79e72ab3b1c3bde826c2ab6cfd8b1a65fda87
Kmenguete <menguetekevin@gmail.com>
2022-06-03 11:45:25 +0200

    adding role to users

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    REVIEWER = 'REVIEWER'
+    REVIEW_ASKER = 'REVIEW_ASKER'
+    ROLE_CHOICES = (
+        (REVIEWER, 'Reviewer'),
+        (REVIEW_ASKER, 'Review_asker'),
+    )
     profile_photo = models.ImageField(verbose_name='profile picture')
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, verbose_name='Role', null=True)

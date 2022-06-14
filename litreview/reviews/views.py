c6eb0b45e42522b5241e59d5f503856e45c908f0
Kmenguete <menguetekevin@gmail.com>
2022-06-14 11:38:40 +0200

    creating the ask review page

--- litreview/reviews/views.py
+++ litreview/reviews/views.py
@@ -5,3 +5,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, 'reviews/home.html')
+
+
+@login_required
+def ask_review(request):
+    return render(request, 'reviews/ask_review.html')

62cbb2257a68e2cf4144ad655ae6aadea6d4dd7d
Kmenguete <menguetekevin@gmail.com>
2022-06-14 15:19:35 +0200

    successfully created link to ask review page

--- litreview/reviews/views.py
+++ litreview/reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from litreview.reviews import forms
+from . import forms
 
 
 @login_required

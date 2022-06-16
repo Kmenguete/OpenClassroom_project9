f74df3a04fa096ac3c285bb4b6fc91babdffff3c
Kmenguete <menguetekevin@gmail.com>
2022-06-16 16:57:16 +0200

    create ask review form

--- litreview/reviews/forms.py
+++ litreview/reviews/forms.py
@@ -6,3 +6,9 @@ class AskReviewForm(forms.ModelForm):
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
+
+
+class CreateReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ['ticket', 'headline', 'rating', 'body']

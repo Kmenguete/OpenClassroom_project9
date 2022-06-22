af71ad3fd34312a3d61dfb38f67b848981473a13
Kmenguete <menguetekevin@gmail.com>
2022-06-22 14:44:07 +0200

    creating a multimodel form in order to let the user to create a review(not in response to a ticket)

--- litreview/reviews/forms.py
+++ litreview/reviews/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . import models
+from betterforms.multiform import MultiModelForm
 
 
 class AskReviewForm(forms.ModelForm):
@@ -19,3 +20,10 @@ class CreateNewReviewForm(forms.ModelForm):
         model = models.Review
         exclude = ('ticket',)
         fields = ['headline', 'rating', 'body']
+
+
+class ReviewNewBookForm(MultiModelForm):
+    form_classes = {
+        'ask_review': AskReviewForm,
+        'create_new_review': CreateNewReviewForm,
+    }

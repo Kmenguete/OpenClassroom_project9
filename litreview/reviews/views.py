67a8ae70bac60f843a2d72817a7c0b4959dbec6a
Kmenguete <menguetekevin@gmail.com>
2022-07-22 15:30:41 +0200

    creating posts page

--- litreview/reviews/views.py
+++ litreview/reviews/views.py
@@ -29,7 +29,12 @@ def exclude_tickets_of_reviews(ticket_of_reviews):
 @permission_required
 def posts(request):
     reviews = models.Review.objects.all()
-    pass
+    ticket_of_reviews = models.Review.objects.filter().values('ticket')
+    real_tickets = exclude_tickets_of_reviews(ticket_of_reviews)
+    tickets_and_reviews = sorted(chain(reviews, real_tickets), key=lambda instance: instance.time_created, reverse=True)
+    return render(request, 'reviews/posts.html',
+                  context={'tickets_and_reviews': tickets_and_reviews, 'reviews': reviews,
+                           'real_tickets': real_tickets})
 
 
 @login_required

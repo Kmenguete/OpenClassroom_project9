604b3c367f694931f79342980b1d22d7bb6f9f45
Kmenguete <menguetekevin@gmail.com>
2022-07-25 15:20:08 +0200

    creating subscriptions view

--- litreview/follow/views.py
+++ litreview/follow/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from litreview.follow.forms import UserFollowForm
 
 
 @login_required
 def subscriptions(request):
-    return render(request, 'follow/subscriptions.html')
+    user_follow_form = UserFollowForm()
+    if request.method == 'POST':
+        user_follow_form = UserFollowForm(request.POST)
+        if user_follow_form.is_valid():
+            return redirect('home')
+    context = {'user_follow_form': user_follow_form}
+    return render(request, 'follow/subscriptions.html', context=context)

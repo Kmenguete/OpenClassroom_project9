89a3770874dcec3370e4900851817370ffb82eb6
Kmenguete <menguetekevin@gmail.com>
2022-07-26 15:53:34 +0200

    if a user A is in the subscrition group of a user B this user B can see the reviews and tickets of user A on her/his home page

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    REVIEWER = 'REVIEWER'
-    REVIEW_ASKER = 'REVIEW_ASKER'
+    SUBSCRIPTION = 'SUBSCRIPTION'
     ROLE_CHOICES = (
-        (REVIEWER, 'Reviewer'),
-        (REVIEW_ASKER, 'Review_asker'),
+        (SUBSCRIPTION, 'SUBSCRIPTION'),
     )
     profile_photo = models.ImageField(verbose_name='profile picture')
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, verbose_name='Role', null=True)

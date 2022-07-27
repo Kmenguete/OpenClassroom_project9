0d27b66fe9a9c5485cac97170b1d03787a74139e
Kmenguete <menguetekevin@gmail.com>
2022-07-27 17:03:09 +0200

    finally I should work with userfollows model

--- litreview/follow/views.py
+++ litreview/follow/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from . import forms
+from . import forms, models
+from ..authentication.models import User
 
 
 @login_required
 def subscriptions(request):
+    user = User.objects.get()
+    followed_user = User.objects.get()
     user_follow_form = forms.UserFollowForm()
     if request.method == 'POST':
         user_follow_form = forms.UserFollowForm(request.POST)

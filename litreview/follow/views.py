ea145c166230740497fc1ebb42e6d2da64aa98b3
Kmenguete <menguetekevin@gmail.com>
2022-07-27 17:11:10 +0200

    finally I don't think I need to work directly with User model(from authentication app)

--- litreview/follow/views.py
+++ litreview/follow/views.py
@@ -2,13 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from . import forms, models
-from ..authentication.models import User
 
 
 @login_required
 def subscriptions(request):
-    user = User.objects.get()
-    followed_user = User.objects.get()
+    user = models.UserFollows.user
+    followed_user = models.UserFollows.followed_user
     user_follow_form = forms.UserFollowForm()
     if request.method == 'POST':
         user_follow_form = forms.UserFollowForm(request.POST)

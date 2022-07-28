a3de7275b7ec5edc78732f51d1f9f157cff621f3
Kmenguete <menguetekevin@gmail.com>
2022-07-28 11:11:37 +0200

    Retrieving users from the database

--- litreview/follow/views.py
+++ litreview/follow/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from . import forms
+from ..authentication.models import User
 
 
 @login_required
@@ -11,10 +12,11 @@ def subscriptions(request):
 
 @login_required
 def search_users(request):
+    user = User.objects.all()
     search_form = forms.SearchForm()
     if request.moethod == 'POST':
         pass
-    context = {'search_form': search_form}
+    context = {'search_form': search_form, 'user': user}
     return render(request, 'follow/subscription.html', context=context)
 
 

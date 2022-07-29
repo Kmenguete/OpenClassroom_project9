26d8c3881a27dcfe3eb12e64bc4802b89a430849
Kmenguete <menguetekevin@gmail.com>
2022-07-29 09:39:55 +0200

    defining the username as the string representation of User object

--- litreview/authentication/models.py
+++ litreview/authentication/models.py
@@ -9,3 +9,6 @@ class User(AbstractUser):
     )
     profile_photo = models.ImageField(verbose_name='profile picture')
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, verbose_name='Role', null=True)
+
+    def __str__(self):
+        return self.username

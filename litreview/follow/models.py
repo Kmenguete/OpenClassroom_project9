a90e93660e2a0ee612077c71cca3ac9362c9d745
Kmenguete <menguetekevin@gmail.com>
2022-08-03 10:42:51 +0200

    I will think about another way to implement Ranga's recommendation

--- litreview/follow/models.py
+++ litreview/follow/models.py
@@ -6,9 +6,6 @@ class UserFollows(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
     followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
 
-    def __str__(self):
-        return self.followed_user
-
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs

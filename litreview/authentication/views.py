48c228d1440be730e5b75434f52d2da3a3027c66
Kmenguete <menguetekevin@gmail.com>
2022-08-12 10:26:00 +0200

    successfully added flash messages for login

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.conf import settings
 from authentication.models import User
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
+from django.views.generic import View
 
 from . import forms
 
@@ -31,17 +32,29 @@ def user_delete(request, id):
     return render(request, 'authentication/user_delete.html', {'user': user})
 
 
-class MyLoginView(LoginView):
+class LoginPageView(View):
     template_name = 'authentication/login.html'
+    login_form = forms.LoginForm
 
-    def form_valid(self, form):
-        user = form.get_user()
-        messages.success(self.request, f'Happy to see you {user.username}')
-        return super().form_valid(form)
+    def get(self, request):
+        form = self.login_form()
+        return render(request, self.template_name, context={'form': form})
 
-    def form_invalid(self, form):
-        messages.error(self.request, 'incorrect username or password.')
-        return super().form_invalid(self)
+    def post(self, request):
+        form = self.login_form(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Happy to see you again {user.username}!')
+                return redirect(settings.LOGIN_REDIRECT_URL)
+            else:
+                messages.error(request, 'Invalid username or password !')
+                return redirect('login')
+        return render(request, self.template_name, context={'form': form})
 
 
 class MyLogoutView(LogoutView):

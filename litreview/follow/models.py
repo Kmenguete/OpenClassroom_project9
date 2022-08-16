da5d8fd181b6a179175e50512e20e6b3cd94f3e4
Kmenguete <menguetekevin@gmail.com>
2022-08-16 19:47:39 +0200

    successfully generated flake8 report

--- litreview/follow/models.py
+++ litreview/follow/models.py
@@ -3,10 +3,19 @@ from django.db import models
 
 
 class UserFollows(models.Model):
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
-    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followed_by')
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following"
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
 
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
-        unique_together = ('user', 'followed_user',)
+        unique_together = (
+            "user",
+            "followed_user",
+        )
--- litreview/follow/views.py
+++ litreview/follow/views.py
@@ -12,51 +12,63 @@ from . import models
 @login_required
 def subscriptions(request):
     follows = models.UserFollows.objects.filter(user=request.user)
-    followers = models.UserFollows.objects.filter(followed_user__username__contains=request.user.username)
-    return render(request, 'follow/subscriptions.html', context={'follows': follows, 'followers': followers})
+    followers = models.UserFollows.objects.filter(
+        followed_user__username__contains=request.user.username
+    )
+    return render(
+        request,
+        "follow/subscriptions.html",
+        context={"follows": follows, "followers": followers},
+    )
 
 
 @login_required
 def follower(request):
     # search_bar = search_users(request)
-    if request.method == 'POST':
-        username = request.POST['username']
+    if request.method == "POST":
+        username = request.POST["username"]
         if username is not None:
             try:
                 user = User.objects.get(username=username)
                 if user == request.user:
                     messages.error(request, "You cannot follow yourself.")
-                    return redirect('subscriptions')
+                    return redirect("subscriptions")
                 else:
-                    user_follows = models.UserFollows.objects.create(user=request.user, followed_user=user)
+                    user_follows = models.UserFollows.objects.create(
+                        user=request.user, followed_user=user
+                    )
                     user_follows.save()
                     messages.success(request, "You follow " + str(username) + ".")
-                    return redirect('subscriptions')
+                    return redirect("subscriptions")
             except User.DoesNotExist:
                 messages.error(request, "The user you are looking for does not exist.")
-                return redirect('subscriptions')
+                return redirect("subscriptions")
             except utils.IntegrityError:
                 messages.error(request, "You already follow " + str(username) + ".")
-                return redirect('subscriptions')
-    return render(request, 'follow/subscriptions.html')
+                return redirect("subscriptions")
+    return render(request, "follow/subscriptions.html")
 
 
 @login_required
 def search_users(request):
-    username = request.GET.get('username')
+    username = request.GET.get("username")
     payload = []
     if username:
         users = User.objects.filter(username__icontains=username)
         for user in users:
             payload.append(user.username)
-    return JsonResponse({'status': 200, 'data': payload})
+    return JsonResponse({"status": 200, "data": payload})
 
 
 def unfollow_user(request, id):
     user = request.user
     followed_user = User.objects.get(id=id)
-    user_follows = models.UserFollows.objects.get(user=user, followed_user=followed_user)
-    if request.method == 'GET':
+    user_follows = models.UserFollows.objects.get(
+        user=user, followed_user=followed_user
+    )
+    if request.method == "GET":
         user_follows.delete()
-        return redirect('subscriptions')
-    return render(request, 'follow/subscriptions.html', context={'user_follows': user_follows})
+        return redirect("subscriptions")
+    return render(
+        request, "follow/subscriptions.html", context={"user_follows": user_follows}
+    )

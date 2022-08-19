0ea7b556df6f05d24995b6469df3325baeda1069
Kmenguete <menguetekevin@gmail.com>
2022-08-19 11:01:02 +0200

    trying to remove create review button of ticket if the ticket has already received a review

--- litreview/reviews/models.py
+++ litreview/reviews/models.py
@@ -9,6 +9,7 @@ class Ticket(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+    is_already_replied = models.BooleanField()
 
 
 class Review(models.Model):

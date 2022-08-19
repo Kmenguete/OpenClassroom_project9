a4db0d694badd92918bf2b80c237d2ec12f32acd
Kmenguete <menguetekevin@gmail.com>
2022-08-19 17:43:44 +0200

    unable to update boolean field of ticket

--- litreview/reviews/models.py
+++ litreview/reviews/models.py
@@ -9,7 +9,7 @@ class Ticket(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
-    is_already_replied = models.BooleanField()
+    is_already_replied = models.BooleanField(default=False, blank=True)
 
 
 class Review(models.Model):
